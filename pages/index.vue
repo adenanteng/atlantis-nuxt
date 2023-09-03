d84a7f3111b8608b3e117455b313e1f4bdf9a029
aden anteng <hey@anteng.co>
2023-09-03 10:34:22 +0700

    the world is primal

--- pages/index.vue
+++ pages/index.vue
@@ -100,15 +100,15 @@
           <button v-if="input !== '' " @click="reset" class="text-sm font-semibold text-gray-900 disabled:text-gray-300">Reset</button>
         </div>
 
-<!--        <iframe class="rounded-3xl mt-10"-->
-<!--                src="https://open.spotify.com/embed/track/3u6PxWema1snbjLbFEyjkJ?utm_source=generator"-->
-<!--                width="100%"-->
-<!--                height="152"-->
-<!--                frameBorder="0"-->
-<!--                allowfullscreen=""-->
-<!--                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"-->
-<!--                loading="lazy">-->
-<!--        </iframe>-->
+        <iframe class="rounded-3xl mt-10"
+                src="https://open.spotify.com/embed/track/3u6PxWema1snbjLbFEyjkJ?utm_source=generator"
+                width="100%"
+                height="152"
+                frameBorder="0"
+                allowfullscreen=""
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy">
+        </iframe>
       </div>
     </div>
 
